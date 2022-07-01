921ebe50f295e5494064475492cb25fbb3066130
gchulev <georgi.chulev@gmail.com>
2022-07-01 08:37:13 +0300

    Added SoftUni problem 'Need for Speed'

---
+++ NeedForSpeed/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var bike = new Motorcycle(80, 22.6);
+            var racebke = new RaceMotorcycle(150, 35.6);
+            var bikeconsumption = bike.FuelConsumption;
+            var raceBikefuelconsumption = racebke.FuelConsumption;
+            var stop = string.Empty;
+        }
+    }
+}
