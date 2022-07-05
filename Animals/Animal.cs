70e7e776bbb3cb52cc0697df39aa667b7b35eb38
gchulev <georgi.chulev@gmail.com>
2022-07-05 13:25:50 +0300

    Added SoftUni problem 'Adnimals'

---
+++ Animals/Animal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string _name;
+        private int _age;
+        private string _gender;
+        protected Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this._name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Name), "Invalid input!");
+                }
+                this._name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this._age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(nameof(this.Name), "Age must be a possitive number!");
+                }
+                this._age = value;
+            }
+        }
+        public string Gender
+        {
+            get
+            {
+                return this._gender;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Gender), "Invalid gender!");
+                }
+                this._gender = value;
+            }
+        }
+
+        public abstract void ProduceSound();
+
+    }
+}
---
+++ Animals/StartUp.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var animalsList = new List<Animal>();
+
+            while (true)
+            {
+                try
+                {
+                    string command = Console.ReadLine();
+                    if (command.Equals("Beast!"))
+                    {
+                        break;
+                    }
+
+                    string[] animalInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                    string name = animalInfo[0];
+                    int age = int.Parse(animalInfo[1]);
+
+                    if (command.Equals("Cat"))
+                    {
+                        string gender = animalInfo[2];
+
+                        Animal cat = new Cat(name, age, gender);
+                        animalsList.Add(cat);
+                    }
+                    else if (command.Equals("Dog"))
+                    {
+                        string gender = animalInfo[2];
+
+                        Animal dog = new Dog(name, age, gender);
+                        animalsList.Add(dog);
+                    }
+                    else if (command.Equals("Frog"))
+                    {
+                        string gender = animalInfo[2];
+
+                        Animal frog = new Frog(name, age, gender);
+                        animalsList.Add(frog);
+                    }
+                    else if (command.Equals("Kitten"))
+                    {
+                        Animal kitten = new Kitten(name, age);
+                        animalsList.Add(kitten);
+                    }
+                    else if (command.Equals("Tomcat"))
+                    {
+                        Animal tomcat = new Tomcat(name, age);
+                        animalsList.Add(tomcat);
+
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Invalid input!");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                    break;
+                }
+                
+            }
+
+            foreach (Animal animal in animalsList)
+            {
+                Console.WriteLine(animal.GetType().Name);
+                Console.WriteLine($"{animal.Name} {animal.Age} {animal.Gender}");
+                animal.ProduceSound();
+            }
+        }
+    }
+}
