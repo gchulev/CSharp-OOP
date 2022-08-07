cd38ebe59229cb876b5d3f5021c35f5ac527caeb
gchulev <georgi.chulev@gmail.com>
2022-08-07 21:21:46 +0300

    Added SoftUni UnitTests

---
+++ Aquariums.Tests/AquariumsTests.cs
@@ -0,0 +1,243 @@
+﻿namespace Aquariums.Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    public class AquariumsTests
+    {
+        [Test]
+        public void PropertyNameThrowsExceptionIfNullOrEmpty()
+        {
+            // Arrange, Act & Assert
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var aquarium = new Aquarium("", 20);
+            }, "Aquarium name is empty but no exception thrown!");
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var aquarium = new Aquarium(null, 20);
+            }, "Aquarium name is null but no exception thrown!");
+        }
+
+        [Test]
+        public void PropertyNameReturnsCorrectName() // very dunb test...but who knows what judge wants
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 20);
+
+            // Act
+            string actualName = aquarium.Name;
+
+            // Assert
+            Assert.That(actualName, Is.EqualTo("test"), $"Actual name is {actualName}, but expected name is 'test'");
+        }
+
+        [Test]
+        public void PopertyCapacityThrowsErrorIfItIsNegative()
+        {
+            // Arrange, Act & assert
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var aquarium = new Aquarium("test", -1);
+            }, "Provided capacity negative value but no exception is thrown!");
+        }
+
+        [Test]
+        public void PropertyCapacityReturnsCorrectValue()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 20);
+
+            // Act
+            int actualCapacity = aquarium.Capacity;
+
+            // Assert
+            Assert.That(actualCapacity, Is.EqualTo(20), $"Actual capacity is {actualCapacity}, but the expected capacity is '20'");
+        }
+
+        [Test]
+        public void PropertyCountReturnsTheCorrectValue()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 20);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+            int actualCount = aquarium.Count;
+
+            // Assert
+            Assert.That(actualCount, Is.EqualTo(2), $"Expected count 2, but actual {actualCount}");
+        }
+
+        [Test]
+        public void AddMethodThrowsExceptionWhenThereISNoSpaceLeftInTheAquarium()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 2);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+            var fish3 = new Fish("fish3");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aquarium.Add(fish3);
+            }, "Fish over capacity but no error thrown!");
+        }
+
+        [Test]
+        public void AddMethodShouldWorkCorrectly()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 2);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+            var fish3 = new Fish("fish3");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+            int actualCount = aquarium.Count;
+
+            // Assert
+            Assert.AreEqual(2, actualCount);
+        }
+
+        [Test]
+        public void RemoveFishMethodShouldThrowErrorIfFishDoesNotExists()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 3);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aquarium.RemoveFish("fish5");
+            }, "Invalid fish name provided but no error was thrown!");
+        }
+
+        [Test]
+        public void RemoveFishMethodRemovesCorrectly() //another meaningless test becuase of judge
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 3);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+            aquarium.RemoveFish("fish1");
+            int actualFishCount = aquarium.Count;
+
+            // Assert
+            Assert.That(actualFishCount, Is.EqualTo(1), $"Expected count 2, but actual {actualFishCount}");
+        }
+
+        [Test]
+        public void SellFishMethodShouldThrowExceptionIfFishNameDoesNotExists()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 3);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aquarium.SellFish("fish5");
+            }, "Fish name doesn't exist but no error is thrown");
+        }
+
+        [Test]
+        public void SellFishMethodShouldReturnCorrectValue()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 3);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+            Fish fish = aquarium.SellFish(fish1.Name);
+
+            // Assert
+            Assert.That(fish.Available, Is.False);
+        }
+
+        [Test]
+        public void ReportMethodShouldProvideCorrectInformation()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 3);
+            var fish1 = new Fish("fish1");
+            var fish2 = new Fish("fish2");
+
+            // Act
+            aquarium.Add(fish1);
+            aquarium.Add(fish2);
+            string actualResult = aquarium.Report();
+
+            // Assert
+            Assert.That(actualResult, Is.EqualTo($"Fish available at test: fish1, fish2"));
+        }
+
+        [Test]
+        public void ReportMethodShouldProvidEmptyInformationIfNoFishAreInTheAquarium()
+        {
+            // Arrange
+            var aquarium = new Aquarium("test", 3);
+
+            // Act
+            string actualResult = aquarium.Report();
+
+            // Assert
+            Assert.That(actualResult, Is.EqualTo($"Fish available at test: "));
+        }
+
+        [TestCase("test1")]
+        [TestCase("test2")]
+        [TestCase("test3")]
+        public void TestIfFishConstructorSetsNamesCorrectly(string name)
+        {
+            // Arrange
+            var fish = new Fish(name);
+
+            // Act & Arrange
+            Assert.AreEqual(name, fish.Name);
+        }
+
+        [Test]
+        public void TestIfAvailablePropertyIsSetCorrectly()
+        {
+            // Arrange
+            var fish = new Fish("test");
+
+            // Act
+            bool actualValue = fish.Available;
+
+            // Assert
+            Assert.IsTrue(actualValue);
+        }
+    }
+}
